eb72b3d8281798993a796fa933cce7e22a66ab3c
TrollTown <williamyinanshen2009@gmail.com>
2019-07-13 13:21:54 +1000

    added dungeonloader to junit test

--- src/unsw/test/Test1.java
+++ src/unsw/test/Test1.java
@@ -5,15 +5,17 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import unsw.dungeon.DungeonControllerLoader;
+
 class Test1 {
 
 	@BeforeEach
 	void setUp() throws Exception {
+		DungeonControllerLoader dungeonLoader = new DungeonControllerLoader("maze.json");
 	}
-
+	
 	@Test
 	void test() {
 		fail("Not yet implemented");
 	}
-
 }
