365d9bd0370ed3a697e211a8454d75b2ec2acb07
TrollTown <williamyinanshen2009@gmail.com>
2019-07-13 13:23:51 +1000

    added dungeon controller to first test

--- src/unsw/test/Test1.java
+++ src/unsw/test/Test1.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import unsw.dungeon.DungeonController;
 import unsw.dungeon.DungeonControllerLoader;
 
 class Test1 {
@@ -12,6 +13,7 @@ class Test1 {
 	@BeforeEach
 	void setUp() throws Exception {
 		DungeonControllerLoader dungeonLoader = new DungeonControllerLoader("maze.json");
+		DungeonController controller = dungeonLoader.loadController();
 	}
 	
 	@Test
